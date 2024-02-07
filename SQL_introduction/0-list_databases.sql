c2463408deffed7f764c717545f2eec3bb7c1bd8
Donkaderon <a.chagdali00@gmail.com>
2024-02-07 21:37:42 +0100

    test5

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,5 +1,3 @@
 -- Connect to the MySQL server
---mysql -u root -p sqlkader18&&
-
 -- List all databases
 SHOW DATABASES;
--- SQL_introduction/0-list_databases1.sql
+++
@@ -1,5 +0,0 @@
--- Connect to the MySQL server
-mysql -u root -p sqlkader18&&
-
--- List all databases
-SHOW DATABASES;
