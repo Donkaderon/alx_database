fce81e4285fc2fbb9d88b24134f37ac09047a9c7
Donkaderon <a.chagdali00@gmail.com>
2024-02-08 21:34:42 +0100

    testp4

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,5 +1,6 @@
 -- Project 4
 -- QUERY to display tables of mysql DATABASE
+
 SET @database_name = 'mysql';
 
 SELECT TABLE_NAME
---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- CREATE NEW TABLE
+
+CREATE TABLE IF NOT EXISTS ${database_name}.first_table (
+    id INT,
+    name VARCHAR(256)
+);
