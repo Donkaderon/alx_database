f52e0bb13f91e6c1a1d168ba5b8025d1a77869ed
Donkaderon <a.chagdali00@gmail.com>
2024-02-09 13:17:04 +0100

    testp5

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,6 @@
+-- Project 5 
+-- a script that prints the full description of the table first_table
+
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
