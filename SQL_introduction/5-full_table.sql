2807161cc0fe775a7265bb65fa7bda1ead3a3111
Donkaderon <a.chagdali00@gmail.com>
2024-02-10 12:24:17 +0100

    tes00

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,4 @@
 -- Project 5 
 -- a script that prints the full description of the table first_table
 
-SELECT *
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'first_table';
+SHOW CREATE TABLE first_table;
