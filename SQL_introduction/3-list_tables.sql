961c0e1c9e103e84ec389be4f69b652cbb5340d2
Donkaderon <a.chagdali00@gmail.com>
2024-02-10 11:11:57 +0100

    ppalltest2

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,7 +1,3 @@
 -- Project 4
 -- QUERY to display tables of a variable DATABASE inserted via command line
-USE your_database_name;
-
-SELECT table_name
-FROM information_schema.tables
-WHERE table_schema = DATABASE();
+SHOW TABLES;
