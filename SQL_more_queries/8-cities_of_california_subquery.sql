b99e0ea3b90e391db0f4cfe0da9650a0e98b4206
Donkaderon <a.chagdali00@gmail.com>
2024-02-11 21:29:03 +0100

    test2p8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,8 @@
 -- PROJECT 8
 -- Cities of California
 
-USE hbtn_0d_usa;
-
 SELECT cities.id, cities.name
-FROM cities
-WHERE state_id = 1;
+FROM cities, states
+WHERE cities.state_id = states.id
+  AND states.name = 'California'
+ORDER BY cities.id ASC;
