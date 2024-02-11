9ff7926186f4766f28ce7f7b1faa94a64ed13040
Donkaderon <a.chagdali00@gmail.com>
2024-02-11 20:38:07 +0100

    testp2

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,11 @@
+-- Project 2
+-- Creation of a read only user
+
+-- Creation of database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Creation of user
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Setting previliges
+GRANT SELECT ON 'hbtn_0d_2' TO 'user_0d_2'@'localhost'; 
