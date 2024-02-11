083edb823ebcda901c23cb72658b8555c174a8f9
Donkaderon <a.chagdali00@gmail.com>
2024-02-11 21:44:13 +0100

    testp03

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -2,6 +2,6 @@
 -- Cities by States
 
 SELECT cities.id, cities.name, states.name
-FROM cities LEFT JOIN states 
-ON cities.states_id= states_id
+FROM cities 
+LEFT JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
